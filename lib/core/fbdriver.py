020b4d2e287d5e72da8764ffd6f9e88f77175149
Curtis Fielding <fieldic@hotmail.com>
2014-05-29 14:18:35 -0700

    created Fbdriver class. Working on test.

--- lib/core/fbdriver.py
+++ lib/core/fbdriver.py
@@ -6,10 +6,14 @@ import json
 from getpass import getpass
 from selenium import webdriver
 
-def login(driver):
-    driver.get('https://www.facebook.com/')
-    assert "Welcome to Facebook" in driver.title
-    creds = getCredentials()
-    driver.find_element_by_id('email').send_keys(creds[0])
-    driver.find_element_by_id('pass').send_keys(creds[1])
-    driver.find_element_by_id('loginbutton').click()
+class Fbdriver(webdriver):
+    def __init__(self):
+        
+        
+    def login(driver):
+        this.get('https://www.facebook.com/')
+        assert "Welcome to Facebook" in this.title
+        creds = getCredentials()
+        this.find_element_by_id('email').send_keys(creds[0])
+        this.find_element_by_id('pass').send_keys(creds[1])
+        this.find_element_by_id('loginbutton').click()
